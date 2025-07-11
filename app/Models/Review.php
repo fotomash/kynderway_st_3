dbea893ef809a06e73ad686b34cfcbe5d7bca5e3
Tom Laskowski <44295192+fotomash@users.noreply.github.com>
2025-07-11 23:54:08 +0100

    Add reviews table and model

---
+++ app/Models/Review.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    protected $table = 'reviews';
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'rating_categories' => 'array',
+        'would_recommend' => 'boolean',
+        'would_hire_again' => 'boolean',
+    ];
+
+    public function reviewer()
+    {
+        return $this->belongsTo(User::class, 'reviewer_id');
+    }
+
+    public function reviewee()
+    {
+        return $this->belongsTo(User::class, 'reviewee_id');
+    }
+}
