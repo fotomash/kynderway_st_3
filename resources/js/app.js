ae2b7683ff756e218463427779c3f10bbc566f31
Tom Laskowski <44295192+fotomash@users.noreply.github.com>
2025-07-12 05:36:42 +0100

    build: optimize webpack output

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,9 @@
 require('./bootstrap');
-
 require('alpinejs');
 
 import { createApp } from 'vue';
 import MapSearch from './components/MapSearch.vue';
 
+const app = createApp({});
+app.component('map-search', MapSearch);
 app.mount('#app');
