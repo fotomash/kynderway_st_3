0e05b6297a4e04a842b464f8b36d851e3ec688a4
Tom Laskowski <44295192+fotomash@users.noreply.github.com>
2025-07-16 15:15:27 +0100

    Add health endpoint and update monitoring

--- routes/metrics.php
+++ routes/metrics.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\MetricsController;
+use App\Http\Controllers\HealthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/metrics', MetricsController::class);
+Route::get('/health', HealthController::class);
